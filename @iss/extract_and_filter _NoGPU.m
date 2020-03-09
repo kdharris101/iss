f8d4e79b2cb4354b70ef43ed72950b33fefded51
Josh Duffield <jduffield65@gmail.com>
2020-03-09 12:48:26 +0000

    Create extract_and_filter _NoGPU.m

---
+++ @iss/extract_and_filter _NoGPU.m
@@ -0,0 +1,234 @@
+function o = extract_and_filter(o)
+% create tiff files for each tile that are top-hat filtered versions of
+% original czi files
+
+    o.TileFiles = cell(o.nRounds+o.nExtraRounds,1,1); % 1,1 because we don't yet know how many tiles
+    
+    %New filter
+    h = -hanning(o.ExtractR2*2+1);
+    h = -h/sum(h);
+    h(o.ExtractR2+1-o.ExtractR1:o.ExtractR2+1+o.ExtractR1) = ...
+        h(o.ExtractR2+1-o.ExtractR1:o.ExtractR2+1+o.ExtractR1)+hanning(o.ExtractR1*2+1)/sum(hanning(o.ExtractR1*2+1));
+    SE = ftrans2(h');
+    SE = single(SE);
+    
+    for r = 1:o.nRounds+o.nExtraRounds       
+        imfile = fullfile(o.InputDirectory, [o.FileBase{r}, o.RawFileExtension]);
+
+        % construct a Bio-Formats reader with the Memoizer wrapper
+        bfreader = loci.formats.Memoizer(bfGetReader(), 0);
+        % initiate reader
+        bfreader.setId(imfile);
+
+        % get some basic image metadata
+        [nSeries, nSerieswPos, nChannels, nZstacks, xypos, pixelsize] = ...
+            get_ome_tilepos(bfreader);
+        if isempty(xypos) || size(xypos, 1)==1
+            if r == 1
+                warning('first round xypos empty - using values from initial manual input')
+                assert(~isempty(o.TileInitialPosYX), 'xypos unavailable')
+                xypos = o.TileInitialPosXY;
+                xyposOld = xypos;
+            else
+                warning('xypos empty - using values from previous round')
+                xypos = xyposOld;
+            end
+            nSerieswPos = size(xypos,1);
+        else
+            xyposOld = xypos;
+        end
+        
+        scene = nSeries/nSerieswPos;
+
+        bfreader.close();
+        
+        if r == 1
+            if isempty(o.AutoThresh)
+                o.AutoThresh = zeros(nSerieswPos,nChannels,o.nRounds+o.nExtraRounds);
+            end
+            if isempty(o.HistCounts)
+                o.HistValues = -o.TilePixelValueShift:1:2^16-o.TilePixelValueShift;  %Entire range of possible pixel values
+                o.HistCounts = zeros(length(o.HistValues),nChannels,o.nRounds);
+            end
+            if isempty(o.TilePosYX)
+                % find x and y grid spacing as median of distances that are about
+                % right
+                dx = xypos(:,1)-xypos(:,1)'; % all pairs of x distances
+                xStep = median(dx(abs(1- dx(:)/o.MicroscopeStepSize)<.5));
+                dy = xypos(:,1)-xypos(:,1)'; % all pairs of y distances
+                yStep = median(dy(abs(1- dy(:)/o.MicroscopeStepSize)<.5));
+        
+        
+                % find coordinates for each tile
+                if isempty(o.TileInitialPosYX)
+                    o.TileInitialPosYX = fliplr(1+round((xypos - min(xypos))./[xStep yStep]));
+                end
+                o.TilePosYX = o.TileInitialPosYX;
+                %Below is a safeguard incase wrong positions found - can do
+                %this as we knwo what the answer should be.
+                MaxY = max(o.TileInitialPosYX(:,1));
+                MaxX = max(o.TileInitialPosYX(:,2));
+                if MaxY*MaxX ~= nSeries
+                    warning('Number of tiles (%d) is not equal to maximum Y position (%d) multiplied by maximum X position (%d)'...
+                        , nSeries, MaxY, MaxX)
+                    break
+                else
+                    TilePosY = flip(repelem(1:MaxY,MaxX));
+                    o.TilePosYX(:,1) = TilePosY;
+                    TilePosX = repmat([flip(1:MaxX),1:MaxX],1,ceil(MaxY/2));
+                    o.TilePosYX(1:nSeries,2) = TilePosX(1:nSeries);
+                end
+            end
+        end
+
+        % set up filename grid for this round
+        fName = cell(nSerieswPos,1);
+        
+        %Set top hat structuring elements
+        if strcmpi(o.DapiR,'auto')
+            o.DapiR = round(8/pixelsize);
+        end
+        DapiSE = strel('disk', o.DapiR);
+        
+        %parfor t = 1:nSerieswPos  
+        for t = 1:nSerieswPos  
+           
+            fName{t} = fullfile(o.TileDirectory, ...
+                    [o.FileBase{r}, '_t', num2str(t), '.tif']);
+            
+            if exist(fName{t}, 'file')
+                fprintf('Round %d tile %d already done.\n', r, t);
+                if o.AutoThresh(t,o.AnchorChannel,r) == 0
+                    TifObj = Tiff(fName{t});
+                    for c=1:nChannels
+                        if c ~= o.AnchorChannel && r == o.ReferenceRound; continue; end
+                        TifObj.setDirectory(o.FirstBaseChannel + c - 1);
+                        IFS = int16(TifObj.read())-o.TilePixelValueShift;
+                        o.AutoThresh(t,c,r) = median(abs(IFS(:)))*o.AutoThreshMultiplier;
+                        if ismember(r,1:o.nRounds)
+                            o.HistCounts(:,c,r) = o.HistCounts(:,c,r)+histc(IFS(:),o.HistValues);
+                        end
+                    end
+                end
+                continue;
+            end                   
+            
+                
+            % a new reader per worker
+            bfreader = javaObject('loci.formats.Memoizer', bfGetReader(), 0);
+            % use the memo file cached before
+            bfreader.setId(imfile);
+
+            bfreader.setSeries(scene*t-1);
+
+            for c = 1:nChannels
+                % read z stacks
+                tic
+                I = cell(nZstacks,1);
+                for z = 1:nZstacks
+                    iPlane = bfreader.getIndex(z-1, c-1, 0)+1;
+                    I{z} = bfGetPlane(bfreader, iPlane);
+                end
+
+                % focus stacking
+                I_mod = o.fstack_modified(I);
+
+                % tophat
+                if c == o.DapiChannel && r == o.ReferenceRound
+                    IFS = imtophat(I_mod, DapiSE);
+                else
+                    I_mod = single(padarray(I_mod,(size(SE)-1)/2,'replicate','both'));
+                    IFS = convn(I_mod,SE,'valid'); 
+                    clearvars I_mod I  %Free up GPU memory
+                    IFS = IFS*o.ExtractScale;
+                    if c ~= o.AnchorChannel && r == o.ReferenceRound
+                        IFS = uint16(IFS+o.TilePixelValueShift); 
+                    else
+                        %Determine auto thresholds
+                        o.AutoThresh(t,c,r) = median(abs(IFS(:)))*o.AutoThreshMultiplier;
+                        if ismember(r,1:o.nRounds)
+                            %Get histogram data
+                            IFS = int16(IFS);
+                            o.HistCounts(:,c,r) = o.HistCounts(:,c,r)+histc(IFS(:),o.HistValues);
+                        end
+                        IFS = uint16(IFS+o.TilePixelValueShift); 
+                    end
+
+                end
+
+                % write stack image
+                imwrite(IFS,...
+                    fullfile(o.TileDirectory,...
+                    [o.FileBase{r}, '_t', num2str(t), '.tif']),...
+                    'tiff', 'writemode', 'append');
+                toc
+            end
+            fprintf('Round %d tile %d finished.\n', r, t);
+            bfreader.close();
+
+        end        
+
+        for t=1:nSerieswPos
+            o.TileFiles{r,o.TilePosYX(t,1), o.TilePosYX(t,2)} = fName{t};
+        end
+    end
+    
+    o.EmptyTiles = cellfun(@isempty, squeeze(o.TileFiles(o.ReferenceRound,:,:)));
+
+    %Plot boxplots showing distribution af AutoThresholds
+    if o.Graphics
+        UseRounds = setdiff(1:o.nRounds+o.nExtraRounds,o.ReferenceRound);
+        Thresholds = [];
+        group = [];
+        index = 1;
+        for c=1:nChannels
+            for r=UseRounds
+                Thresholds = [Thresholds;o.AutoThresh(:,c,r)];
+                group = [group;index*ones(size(o.AutoThresh(:,1,1)))];
+                index = index+1;
+            end
+        end
+        %Add anchor
+        AnchorLabel = {'Anchor'};
+        Thresholds = [Thresholds;o.AutoThresh(:,o.AnchorChannel,o.ReferenceRound)];
+        group = [group;index*ones(size(o.AutoThresh(:,1,1)))];
+
+        
+        figure(43290);
+        colors = colormap(lines(nChannels));
+        Colors = repelem(colors,length(UseRounds),1);
+        Colors = [Colors;repelem([0,0,0],nChannels,1)];
+        Labels = [string(repmat(UseRounds,1,nChannels)),string(AnchorLabel)];
+        boxplot(Thresholds,group,'Colors',Colors, 'plotstyle', 'compact','labels', Labels);
+        set(gca,'TickLength',[0 0]);
+        ylabel('AutoThreshold');
+        xlabel('Round');
+        hold on
+        for c=1:nChannels
+            plot(NaN,1,'color', colors(c,:), 'LineWidth', 4);       %For legend labels
+        end
+        leg = legend(o.bpLabels,'Location','northwest');
+        title(leg,'Color Channel');
+        hold off
+    end
+    
+    %Plot histograms to make sure they are smooth
+    %Avoid ExtraRounds as only need histograms for the 7 rounds used to
+    %define genes
+    if o.Graphics       
+        figure(43291);
+        index = 1;
+        for r=1:o.nRounds
+            for b=1:nChannels
+                subplot(o.nRounds,nChannels,index)
+                histogram('BinEdges',[o.HistValues-0.5,max(o.HistValues)+0.5],'BinCounts',o.HistCounts(:,b,r),'DisplayStyle','stairs');
+                xlim([-1000,1000]);
+                ylim([0,max(o.HistCounts(:,b,r))]);
+                if b==4
+                    title(strcat('Round ',num2str(r)));
+                end
+                index = index+1;
+            end
+        end
+    end
+end
